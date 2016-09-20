50ef8dd0f11b67a2c8e5ab9c63e92366569652e3
willnickerson <wnickers@willamette.edu>
2016-09-20 16:21:30 -0700

    projectSource array is sorted and rendered to page

--- scripts/projects.js
+++ scripts/projects.js
@@ -14,6 +14,35 @@ function Project(opts) {
 
 //This function will take a Project object, convert it to Html and append it to the projects section of our page.
 Project.prototype.toHtml = function () {
+  //cloning template article and converting to jQuery object.
   var $newProject = $('article.template').clone();
+
+  //assigning Project attributes to their corresponing html elements in our cloned template.
+  $newProject.attr('data-category', this.category);
+  $newProject.find('h4 a').text(this.title).attr('href', this.projectUrl);
+  $newProject.find('h5').text('By ' + this.authors);
+  $newProject.find('img.project-img').attr('src', this.img);
+  $newProject.find('section.project-description').html(this.description);
+
+  //removing template class so that project will display
+  $newProject.removeClass('template');
+
   return $newProject;
 };
+
+//TODO make function to sort projects in order of importance
+
+userData.sort(function(currentElement, nextElement) {
+  return currentElement.importance - nextElement.importance;
+});
+//using the objects form the array in projectsSource to populate our projects array with new Project objects.
+
+userData.forEach(function(p) {
+  projects.push(new Project(p));
+});
+
+//taking the elements from our newly populated projects array and converting them to html and appending them to the section with id="projects"
+
+projects.forEach(function(project){
+  $('#projects').append(project.toHtml());
+});
--- scripts/projectsSource.js
+++ scripts/projectsSource.js
@@ -1,8 +1,10 @@
 'use strict';
 
+//The importance attribute will be used to determine which projects jet rendered at the top of the page. Index the projects you want displayed first starting at 0.
 var userData = [
   {
     title: 'test',
+    importance: 2,
     category: 'test',
     authors: 'test and test',
     projectUrl: 'http://www.google.com',
@@ -12,6 +14,27 @@ var userData = [
 
   {
     title: 'test 2',
+    importance: 0,
+    category: 'test',
+    authors: 'test and test',
+    projectUrl: 'http://www.google.com',
+    img: 'http://placehold.it/200x200',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+
+  {
+    title: 'test 3',
+    importance: 1,
+    category: 'test',
+    authors: 'test and test',
+    projectUrl: 'http://www.google.com',
+    img: 'http://placehold.it/200x200',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+
+  {
+    title: 'test 4',
+    importance: 4,
     category: 'test',
     authors: 'test and test',
     projectUrl: 'http://www.google.com',
