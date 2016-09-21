50bb5aae84a31aacf0700a54a816c9803befe358
willnickerson <wnickers@willamette.edu>
2016-09-20 17:04:55 -0700

    project info added

--- scripts/projectsSource.js
+++ scripts/projectsSource.js
@@ -3,38 +3,29 @@
 //The importance attribute will be used to determine which projects jet rendered at the top of the page. Index the projects you want displayed first starting at 0.
 var userData = [
   {
-    title: 'test',
-    importance: 2,
-    category: 'test',
-    authors: 'test and test',
-    projectUrl: 'http://www.google.com',
-    img: 'http://placehold.it/200x200',
+    title: 'Bus-Mall',
+    importance: 1,
+    category: 'web app',
+    authors: 'Will Nickerson',
+    projectUrl: 'https://willnickerson.github.io/bus-mall/',
+    img: 'imgs/portfolio-bus-mall.png',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   },
 
   {
-    title: 'test 2',
+    title: 'Yachtzoe!',
     importance: 0,
-    category: 'test',
-    authors: 'test and test',
-    projectUrl: 'http://www.google.com',
-    img: 'http://placehold.it/200x200',
+    category: 'web app',
+    authors: 'John Gothro, Risa Fletcher, and Will Nickerson',
+    projectUrl: 'https://jpgothro.github.io/yachtzoe_project/index.html',
+    img: 'imgs/portfolio-yachtzoe.png',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   },
 
+//this will be a placeholder until I get another project in here.
   {
     title: 'test 3',
     importance: 1,
-    category: 'test',
-    authors: 'test and test',
-    projectUrl: 'http://www.google.com',
-    img: 'http://placehold.it/200x200',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  },
-
-  {
-    title: 'test 4',
-    importance: 4,
     category: 'test',
     authors: 'test and test',
     projectUrl: 'http://www.google.com',
