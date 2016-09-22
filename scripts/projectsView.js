abf8a21318b23c8856a71c6fbb11769e98a7bc24
willnickerson <wnickers@willamette.edu>
2016-09-22 16:16:35 -0700

    project filter working

--- scripts/projectsView.js
+++ scripts/projectsView.js
@@ -2,6 +2,7 @@
 
 var projectView = {};
 
+//this takes user to part of desired part of the page via the nav bar
 projectView.handleNav = function() {
   $('nav').on('click', '.nav-tab', function(){
     var $selectedContent = $(this).attr('data-type');
@@ -12,6 +13,16 @@ projectView.handleNav = function() {
   });
 };
 
+projectView.handleFilter = function() {
+  $('#filter').on('change', function() {
+    if($(this).val()) {
+      $('.project-article').fadeOut(500);
+      $('article[data-category="' + $(this).val() + '"]').fadeIn(500);
+    }
+  });
+};
+
+//this is used to hide the aricle text until the user clicks read-more
 projectView.preview = function() {
   $('.project-description *:nth-of-type(n+2)').hide();
   $('a.read-more').on('click', function(e) {
@@ -20,10 +31,11 @@ projectView.preview = function() {
     $(this).text('Show less');
     $(this).removeClass('read-more');
     $(this).addClass('show-less');
-  //TODO add code for show-less  
+  //TODO add code for show-less
   });
 };
 
 
 projectView.handleNav();
+projectView.handleFilter();
 projectView.preview();
