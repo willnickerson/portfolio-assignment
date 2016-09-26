8d1320117f1adaf4692d0fd0174fd4af183ca406
willnickerson <wnickers@willamette.edu>
2016-09-26 16:48:08 -0700

    asyncronous page rendering works

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,6 +1,6 @@
 'use strict';
 //This array will store our project objects.
-var projects = [];
+// var projects = [];
 
 //Constructor function for Projects objects. Takes an object from userData array and makes it into new Project.
 function Project(opts) {
@@ -11,6 +11,8 @@ function Project(opts) {
   this.img = opts.img;
   this.description = opts.description;
 }
+
+Project.all = [];
 
 //This function will take a Project object, convert it to Html using handlebars so that it can be appened to the projects section of our page.
 
@@ -28,20 +30,42 @@ Project.prototype.toFilter = function() {
   return html;
 };
 
-//this sorts the projects by their importance attributes
-userData.sort(function(currentElement, nextElement) {
-  return currentElement.importance - nextElement.importance;
-});
+Project.loadAll = function(passedData) {
+  //this sorts the projects by their importance attributes and then pushes them to Project.all array.
+  passedData.sort(function(currentElement, nextElement) {
+    return currentElement.importance - nextElement.importance;
+  }).forEach(function(p) {
+    Project.all.push(new Project(p));
+  });
+};
 
-//using the objects form the array in projectsSource to populate our projects array with new Project objects.
-userData.forEach(function(p) {
-  projects.push(new Project(p));
-});
-
-//taking the elements from our newly populated projects array and converting them to html and appending them to the section with id="projects"
-projects.forEach(function(project){
-  $('#projects').append(project.toHtml());
-  if($('#filter option[value="' + project.category + '"]').length === 0) {
-    $('#filter').append(project.toFilter());
-  };
-});
+//This function will retrieve data from local storage or the json file and render it to index.html
+Project.retrieveAll = function() {
+  if(localStorage.projects) {
+    //If there are projects saved in local storage retrieve them, then parse them and render to page.
+    console.log('we found stuff');
+    var retrievedProjects = localStorage.getItem('projects');
+    var parsedProjects = JSON.parse(retrievedProjects);
+    console.log(parsedProjects);
+    Project.loadAll(parsedProjects);
+    projectView.renderToIndex();
+  } else {
+    //take proejects from projects.json, append them to page and add them to local storage
+    $.ajax('data/projects.json', {
+      method: 'GET',
+      success: successHandler,
+      error: errorHandler
+    });
+    function successHandler(data) {
+      //take the data from json file and load to Project.all then render Project.all to index.html
+      Project.loadAll(data);
+      projectView.renderToIndex();
+      //stringify data from JSON file and add to local storage
+      var projectsString = JSON.stringify(data);
+      localStorage.setItem('projects', projectsString);
+    };
+    function errorHandler(error) {
+      console.log('ERROR', error);
+    };
+  }
+};
